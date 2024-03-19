362dd159a2653b07cdd1160c36c8dc0f5f8422ec
Maycol <maycolquispecaccha@gmail.com>
2024-03-19 17:53:20 -0500

    Add interaction whit the DOM

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // console.log("dom");
 
 // const dom1 = document.querySelector("#item1");
+// dataset
+// console.log(dom1.dataset.custom);
 // const dom1 = document.querySelectorAll(".grid-item");
 // const dom1 = document.querySelectorAll(".grid-item");
-const dom1 = document.querySelector(".grid-item");
+// const dom1 = document.querySelector(".grid-item");
 // console.dir(dom1);
 // dom1.textContent = "World";
 // dom1.style.backgroundColor = "red";
@@ -11,9 +13,37 @@ const dom1 = document.querySelector(".grid-item");
 
 //dataset
 // console.log(dom1.dataset);
-const div = document.createElement("div");
-div.classList.add("grid-item");
-div.textContent = "7";
-console.log(div);
+
+//Modificar el DOM
+// const div = document.createElement("div");
+// div.classList.add("grid-item");
+// div.textContent = "7";
+// console.log(div);
+// const grid = document.querySelector(".grid");
+// grid.append(div);
+
+//Usando el innerHTML
+// const div8 = `<div class="grid-item" data-color="red">8</div>`;
+// grid.innerHTML = grid.innerHTML + div8;
+// console.log(grid.innerHTML);
+
+//Forms
+// const form = document.querySelector("form");
+// console.log(form);
+// const newInput = `<input type="password" />`;
+// form.innerHTML = form.innerHTML + newInput;
+//events
+const dom1 = document.querySelector("#item1");
+// dom1.addEventListener("click", (event) =>
+//   console.log((event.target.style.backgroundColor = "red"))
+// );
+// Event delegation
+const handleClick = (event) => {
+  if (event.target.classList.contains("grid-item")) {
+    console.log(
+      (event.target.style.backgroundColor = event.target.dataset.color)
+    );
+  }
+};
 const grid = document.querySelector(".grid");
-grid.append(div);
+grid.addEventListener("click", handleClick);
